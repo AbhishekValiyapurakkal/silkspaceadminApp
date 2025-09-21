b5b2c25435c82196c51055d730d52a68680ddb93
github_AbhishekValiyapurakkal <abhiv6160@gmail.com>
2025-09-21 11:34:41 +0530

    added splash screen with login logic

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:silkspaceadmin/domain/firebase_options.dart';
 import 'package:silkspaceadmin/global_variables.dart';
-import 'package:silkspaceadmin/presentation/home/Homepage.dart';
-import 'package:silkspaceadmin/presentation/login/Loginpage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:silkspaceadmin/presentation/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,52 +16,7 @@ void main() async {
     ],
     child: const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: First(),
+      home: SplashScreen(),
     ),
   ));
-}
-
-class First extends StatefulWidget {
-  const First({super.key});
-
-  @override
-  State<First> createState() => _FirstState();
-}
-
-class _FirstState extends State<First> {
-  bool finalstate = false;
-  Future getloginstate() async {
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    bool? state = preferences.getBool('login');
-    setState(() {
-      finalstate = state!;
-    });
-  }
-
-  @override
-  void initState() {
-    getloginstate().whenComplete(
-      () {
-        if (finalstate == false) {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const Loginpage(),
-              ));
-        } else {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const homepage(),
-              ));
-        }
-      },
-    );
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
